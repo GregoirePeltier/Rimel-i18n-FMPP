8126dce2e77493e9ffd54faf32b23202751873f6
Prune Pillone <prune6@gmail.com>
2020-01-30 18:55:02 +0100

    #2 Modify script in order to look for all commits impacted by l10n

--- gitStudy/git_commit.py
+++ gitStudy/git_commit.py
@@ -1,7 +1,6 @@
 # Source https://gist.github.com/simonw/091b765a071d1558464371042db3b959
 # Modified by prune.pillone@etu.unice.fr
 
-## to run the script you need to put the path to the git root
 import subprocess
 import re
 import os
@@ -11,10 +10,36 @@ leading_4_spaces = re.compile('^    ')
 
 keywords = ["localization", "l10n", "i18n", "internationalization", "translate", "translation", "weblate"]
 
+
 def getName():
-    list = sys.argv[1].split("/")
-    filename = "output_" + list[len(list)-2] + ".txt"
-    return filename
+    list = os.getcwd().split("/")
+    print(list[len(list) - 1])
+    projectName = list[len(list) - 1]
+    return projectName
+
+
+def run():
+    outputFile = open("./output/output.txt", 'w')
+    for f in os.walk("../Projects"):
+        for folder in f[1]:
+            os.chdir("../Projects/" + folder)
+            commits = get_commits()
+            total = 0
+            totalLoc = 0
+            for commit in commits:
+                total += 1
+                # print(commit['title'])  print(commit['message']) print(commit['hash'])
+                found = 0
+                for word in keywords:
+                    if found == 0 and (word in commit['message'] or word in commit['title']):
+                        # Pour récupérer le nombre de fichiers impactés par commit
+                        # get_Files(commit, outputFile)
+                        totalLoc += 1
+                        found = 1
+            outputFile.write(getName() + " = " + str(totalLoc) + "/" + str(total) + "\n")
+            os.chdir("..")
+    outputFile.close()
+
 
 def get_commits():
     lines = subprocess.check_output(
@@ -24,13 +49,14 @@ def get_commits():
     current_commit = {}
 
     def save_current_commit():
-        title = current_commit['message'][0]
-        message = current_commit['message'][1:]
-        if message and message[0] == '':
-            del message[0]
-        current_commit['title'] = title
-        current_commit['message'] = '\n'.join(message)
-        commits.append(current_commit)
+        if 'message' in current_commit:
+            title = current_commit['message'][0]
+            message = current_commit['message'][1:]
+            if message and message[0] == '':
+                del message[0]
+            current_commit['title'] = title
+            current_commit['message'] = '\n'.join(message)
+            commits.append(current_commit)
 
     for line in lines:
         if not line.startswith(' '):
@@ -53,7 +79,8 @@ def get_commits():
         save_current_commit()
     return commits
 
-def get_Files(commit):
+
+def get_Files(commit, outputFile):
     number = 0
     files = subprocess.check_output(
         ['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', commit['hash']], stderr=subprocess.STDOUT).split(
@@ -61,20 +88,7 @@ def get_Files(commit):
     for file in files:
         # print(file)
         number += 1
-    outputFile.write(commit['hash'] + "->" + commit['title'] + " : " + commit['message'] + " = " + str(number) + "\n")
+    outputFile.write(commit['hash'] + " = " + str(number) + "\n")
 
-outputFile = open(getName(), 'w')
-os.chdir(sys.argv[1])
-commits = get_commits()
 
-for commit in commits:
-    # print(commit['title'])
-    # print(commit['message'])
-    # print(commit['hash'])
-    found = 0
-    for word in keywords:
-        if found == 0 and (word in commit['message'] or word in commit['title']):
-            get_Files(commit)
-            found = 1
-
-outputFile.close()
+run()
