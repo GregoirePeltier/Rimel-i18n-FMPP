7d1d63868af36fa4bd624dc88d1d31037e89f171
Hugo Francois <hugo.ojvind.francois@gmail.com>
2020-02-11 11:50:22 +0100

    #22 Add more stat

--- weblate/look_for_key.py
+++ weblate/look_for_key.py
@@ -1,4 +1,5 @@
 import json
+from collections import Counter
 
 from path import Path
 import re
@@ -60,83 +61,131 @@ androidProjects = [
 ]
 
 keyInFile = {}
-listOfFile = []
+totalNbFile = 0
+totalNbFileWithKey = 0
+javaFileNb = 0
+cnt = Counter()
 for f in Path('../Projects/unity').walkfiles():
 	if patternForJava.match(f):
 		# print(f)
+		javaFileNb += 1
 		file = open(f, "r")
 		for line in file:
 			if patternForUnity.search(line):
-				listOfFile.append(f)
-				# if len(listOfFile) == 0:
-				# 	listOfFile.append({f: 0})
-				# else:
-				# 	alreadyExist = False
-				# 	for item in listOfFile:
-				# 		if item[f] is not None:
-				# 			item[f] = item[f] + 1
-				# 			alreadyExist = True
-				# 			break
-				# 	else:
-				# 		listOfFile.append({f: 1})
+				cnt[f] += 1
 				print(f)
 
-keyInFile['../Projects/unity'] = listOfFile.copy()
+keyInFile['../Projects/unity'] = {
+	"totalNbFile": javaFileNb,
+	"listOfFileWithKey": cnt.copy(),
+	"totalNbFileWithKey": len(cnt),
+	"average": (len(cnt) / javaFileNb) * 100
+}
 
-listOfFile.clear()
+totalNbFile += javaFileNb
+totalNbFileWithKey += len(cnt)
+jspFileNb = 0
+cnt = Counter()
 
 for f in Path('../Projects/airsonic').walkfiles():
 	if patternForJsp.match(f):
 		# print(f)
+		jspFileNb += 1
 		file = open(f, "r")
 		for line in file:
 			if patternForKey.search(line):
-				listOfFile.append(f)
+				cnt[f] += 1
 				print(f)
 
-keyInFile['../Projects/airsonic'] = listOfFile.copy()
+keyInFile['../Projects/airsonic'] = {
+	"totalNbFile": jspFileNb,
+	"listOfFileWithKey": cnt.copy(),
+	"totalNbFileWithKey": len(cnt),
+	"average": (len(cnt) / jspFileNb) * 100
+}
 
-listOfFile.clear()
-
+totalNbFile += javaFileNb
+totalNbFileWithKey += len(cnt)
+javaFileNb = 0
+cnt = Counter()
 for f in Path('../Projects/freeplane').walkfiles():
 	if patternForJava.match(f):
+		javaFileNb += 1
 		# print(f)
 		file = open(f, "r")
 		for line in file:
 			if patternForFreePlane.search(line):
-				listOfFile.append(f)
+				cnt[f] += 1
 				print(f)
 
-keyInFile['../Projects/freeplane'] = listOfFile.copy()
+keyInFile['../Projects/freeplane'] = {
+	"totalNbFile": javaFileNb,
+	"listOfFileWithKey": cnt.copy(),
+	"totalNbFileWithKey": len(cnt),
+	"average": (len(cnt) / javaFileNb) * 100
+}
 
-listOfFile.clear()
-
-
+totalNbFile += javaFileNb
+totalNbFileWithKey += len(cnt)
+javaFileNb = 0
+cnt = Counter()
 for f in Path('../Projects/RouteConverter').walkfiles():
 	if patternForJava.match(f):
+		javaFileNb += 1
 		# print(f)
 		file = open(f, "r")
 		for line in file:
 			if patternForRouteConverter.search(line):
-				listOfFile.append(f)
+				cnt[f] += 1
 				print(f)
 
-keyInFile['../Projects/RouteConverter'] = listOfFile.copy()
+keyInFile['../Projects/RouteConverter'] = {
+	"totalNbFile": javaFileNb,
+	"listOfFileWithKey": cnt.copy(),
+	"totalNbFileWithKey": len(cnt),
+	"average": (len(cnt) / javaFileNb) * 100
+}
 
-listOfFile.clear()
 
+
+totalNbFile += javaFileNb
+totalNbFileWithKey += len(cnt)
+
+keyInFile["Java stat"] = {
+	"totalNbFile": totalNbFile,
+	"totalNbFileWithKey": totalNbFileWithKey,
+	"average": (totalNbFileWithKey / totalNbFile) * 100
+}
+
+javaFileNb = 0
+cnt = Counter()
+totalNbFile = 0
+totalNbFileWithKey = 0
 for s in androidProjects:
 	for f in Path(s).walkfiles():
 		if patternForJava.match(f):
+			javaFileNb += 1
 			# print(f)
 			file = open(f, "r", encoding="utf8")
 			for line in file:
 				if patternForAndroidReference.search(line):
-					listOfFile.append(f)
+					cnt[f] += 1
 					print(f)
-	keyInFile[s] = listOfFile.copy()
-	listOfFile.clear()
-
+	keyInFile[s] = {
+		"totalNbFile": javaFileNb,
+		"listOfFileWithKey": cnt.copy(),
+		"totalNbFileWithKey": len(cnt),
+		"average": (len(cnt) / javaFileNb) * 100
+	}
+	totalNbFile += javaFileNb
+	totalNbFileWithKey += len(cnt)
+	javaFileNb = 0
+	cnt.clear()
+keyInFile["Android stat"] = {
+	"totalNbFile": totalNbFile,
+	"totalNbFileWithKey": totalNbFileWithKey,
+	"average": (totalNbFileWithKey / totalNbFile) * 100
+}
 
 f = open("result_key_in_java.json", "w")
 # for folder in keyInFile:
